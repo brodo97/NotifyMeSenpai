1a1b2fb6d75aa16e353bbe79bb7ccbaba89ccf46
mattia <brodo97nosense@gmail.com>
2022-01-31 18:30:22 +0100

    Created a first iteration of the Updater

---
+++ Updater/Updater.py
@@ -0,0 +1,120 @@
+import sqlite3
+from datetime import datetime
+import requests
+from bs4 import BeautifulSoup
+import sys
+import time
+import traceback
+
+conn = sqlite3.connect('Database.db', check_same_thread=False)
+check_every = 60
+
+
+def get_skip_languages(chat_ids, previous_settings):
+    # For every chat's ID following this Group/Artist/Category/Character
+    for chat_id in chat_ids.split(','):
+        # Check if it has been already checked/updated/inserted in the previous settings variable
+        if chat_id in previous_settings:
+            continue
+        # Get skip_languages from Settings table (if exists)
+        languages = conn.execute(
+            f'SELECT SettingValue FROM Settings WHERE ChatID == {chat_id} AND Setting LIKE \'skip_languages\''
+        ).fetchone()
+
+        # And if not exists then []
+        languages = languages[0].split(',') if languages is not None else []
+
+        # Update previous settings for later return
+        previous_settings.update({chat_id: languages})
+    return previous_settings
+
+
+# Creating requests' Session
+with requests.Session() as SESSION:
+    while 1:
+        Settings = {}
+        SleepTime = 3600
+        for row in conn.execute('SELECT * FROM Data'):
+            ID, ChatIDs, Link, Name, KnownWorks, LastCheck = row
+            LastCheck = datetime.strptime(LastCheck, '%Y/%m/%d %H:%M:%S')
+
+            # Update Settings variable. (Every hour or so)
+            NewSettings = get_skip_languages(ChatIDs, Settings)
+            Settings.update(NewSettings)
+
+            # Init to arbitrary datetime
+            if LastCheck is None:
+                LastCheck = datetime(2020, 1, 1)
+
+            # If it was checked less than an hour ago, skip
+            LastCheckSeconds = (datetime.now() - LastCheck).total_seconds()
+            if LastCheckSeconds < 3600:
+                # Calculate sleep till next check cycle
+                if 3600 - LastCheckSeconds < SleepTime:
+                    SleepTime = 3600 - LastCheckSeconds
+
+                    # Check if SleepTime > 0. Don't want negative sleep
+                    SleepTime = 0 if SleepTime < 0 else SleepTime
+                continue
+
+            time.sleep(2)  # Sleep 2 seconds for spam prevention
+
+            RESULT = SESSION.get(Link)  # Do a request to the link using current session
+
+            # If the response's status code differ from 200, continue
+            if RESULT.status_code != 200:
+                print(f'ERROR - check_nhentai(): {Link} returned status code = {RESULT.status_code}')
+                continue
+
+            SOUP = BeautifulSoup(RESULT.content, 'html.parser')  # Pass response's content to BeautifulSoup
+
+            # If response's content is malformed, continue
+            if SOUP is None:
+                print(f'ERROR - check_nhentai(): BeautifulSoup error')
+                continue
+
+            PendingTransaction = False  # Using this for rollback in case of an exception
+            try:
+                # For every work panel/div
+                for gallery_div in SOUP.find_all('div', 'gallery'):
+                    # If is known, continue
+                    if gallery_div.find('a')['href'] in KnownWorks:
+                        continue
+
+                    # Current div link, es: /g/000000
+                    WorkLink = gallery_div.find('a')['href']
+
+                    # Work's link insertion in the database for duplication avoidance
+                    conn.execute(f'UPDATE Data SET KnownWorks = KnownWorks || ",{WorkLink}" WHERE ID == {ID};')
+                    PendingTransaction = True
+
+                    # Current div category, es: Artist, Category, Group
+                    category = Link.split('/')[3].title()
+
+                    # Constructing message's content. Will be sent from Telegram to the users
+                    text = f'New {category} work: [{Name}](https://nhentai.net{WorkLink})]'
+
+                    # For every chat's ID following this Group/Artist/Category/Character
+                    for ChatID in ChatIDs.split(','):
+                        # Check if the user is avoiding a specific languages
+                        if any([language in gallery_div['data-tags'] for language in Settings[ChatID]]):
+                            continue
+                        # Else, insert in the database a new message to send
+                        conn.execute(
+                            f'INSERT INTO MESSAGES (ChatID, Content) VALUES ({ChatID}, \'{text}\');'
+                        )
+                    else:
+                        break  # Breaking when finding a duplicate
+
+                # Updating LastCheck to current datetime and update relative row
+                LastCheck = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+                conn.execute(f'UPDATE Data SET LastCheck = \'{LastCheck}\' WHERE ID == {ID};')
+
+                conn.commit()  # Commit changes
+            except Exception:
+                # If there were pending transaction, rollback. Maybe it will work at next cycle
+                if PendingTransaction is True:
+                    conn.execute('rollback')
+                traceback.print_exc()
+
+        time.sleep(SleepTime)
